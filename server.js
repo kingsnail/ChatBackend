efb725019d687a63bc106daef2e1a5bb89e01c45
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-08-08 20:46:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.post('/register', (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(err => {
+    if (err) {
+      res.status(400).send('Error registering user.');
+    } else {
+      res.status(200).send('User registered successfully!');
+    }
+  });
+});
+
+app.listen(5000, () => {
+  console.log('Server started on port 5000');
+});
 
