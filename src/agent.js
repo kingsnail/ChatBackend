69ef9e693c6cc88b8c374c3e31fdbd71b279917d
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-08-09 22:10:52 +0300

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -1,9 +1,19 @@
+const { v4: uuidv4 } = require('uuid');
+
 // Parent class for generic agent type
 class Agent {
     constructor(name) {
-        this.name = name;
+        let this.name = name;
+        let this.uuid = uuidv4();
+        let this.subscribers = [];
+        
+        console.log(`New agent ${this.name} UUID: €{this.uuid}`);
     }
 
+    getUUID() {
+        return this.uuid;
+    }
+    
     setInput(i) { 
         console.log('Input is ${i}.);  
     }
@@ -12,7 +22,17 @@ class Agent {
         console.log(`${this.name} makes a noise.`);
     }
 
-    subscribe(cb) {
+    subscribe(s) {
+        this.subscribers.push(s);
         console.log('Subscribe');
+    }
+
+    load(d){
+        console.log('Load');
+    }
+
+    save(){
+        console.log('Save');
+        return "Saved data";
     }
 }
