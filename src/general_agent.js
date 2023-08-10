a0a83f9be561447625518543c8d600c9ffded226
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-08-10 20:33:38 +0300

    Update general_agent.js

--- src/general_agent.js
+++ src/general_agent.js
@@ -1,12 +1,8 @@
 // Parent class for generic agent type
 class GeneralAgent extends Agent {
     constructor(name, displayLocation) {
-        let this.agentType = "GeneralAgent";
-        let this.name = name;
-        let this.uuid = uuidv4();
-        let this.subscribers = [];
-        let this.displayLocation = displayLocation;
-        
+        super(name, displayLocation);
+        let this.agentType = "GeneralAgent";        
         console.log(`New general agent ${this.name} UUID: €{this.uuid}`);
     }
 }
