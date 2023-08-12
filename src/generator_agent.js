9027173c02c434cf3ccfe0e19d8513f6fcfc2df9
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-08-12 19:28:41 +0300

    Update generator_agent.js

--- src/generator_agent.js
+++ src/generator_agent.js
@@ -1,1 +1,11 @@
+const Agent = require('./agent');
 
+class GeneratorAgent extends Agent {
+    constructor(name, displayLocation) {
+        super(name, displayLocation);
+        this.agentType = "GeneratorAgent";        
+        console.log(`New generator agent ${this.name} UUID: €{this.uuid}`);
+    }
+}
+
+module.exports = GenetorAgent;
