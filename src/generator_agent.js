c72c20d2c2ecc98fec04f8c834c8ca480b12ec9a
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-08-13 13:03:51 +0300

    Update generator_agent.js

--- src/generator_agent.js
+++ src/generator_agent.js
@@ -6,6 +6,28 @@ class GeneratorAgent extends Agent {
         this.agentType = "GeneratorAgent";        
         console.log("New generator agent " + this.name + " UUID: " + this.uuid);
     }
+        
+    getSystemPrompt() {
+        return this.systemPrompt;
+    }
+    getUserPrompt() {
+        return this.userPrompt;
+    }
+    getOutputPrompt() {
+        return this.outputPrompt;
+    }
+    
+    setSystemPrompt(p) {
+        this.systemPrompt = p;
+    }
+    setUserPrompt(p) {
+        this.userPrompt = p;
+    }
+    setOutputPrompt(p) {
+        this.outputPrompt = p;
+    }
+
+
 }
 
 module.exports = GeneratorAgent;
