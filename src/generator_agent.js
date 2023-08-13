c3c3d55c1e18170bfe4aaaf64a3dc044f46933bf
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-08-13 19:32:07 +0300

    Update generator_agent.js

--- src/generator_agent.js
+++ src/generator_agent.js
@@ -4,9 +4,14 @@ class GeneratorAgent extends Agent {
     constructor(name, displayLocation) {
         super(name, displayLocation);
         this.agentType = "generator-agent";   
-        this.systemPrompt = "";
+        this.systemPrompt = 'You are a helpful assistant.  
+             Please try and follow the next instructions to the best of your ability. 
+             When asked a question, answer the question to the best of your ability.';
         this.userPrompt = "";
-        this.outputPrompt = "";
+        this.outputPrompt = `Format your output as a JSON object with a key value of 'chatResult'. 
+             Format 'chatResult' as a JSON list for each response item listed where each list item has the following tags: 
+             'item' whos value is the serial number of the item; and
+             'text' whos value is the text of the item.`;
         console.log("New generator agent " + this.name + " UUID: " + this.uuid);
     }
         
