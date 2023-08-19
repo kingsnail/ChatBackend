8cf7cdc7b34e1dd9a01021dd50375ca560d0c8e7
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-08-19 13:47:37 +0300

    Update ChatGPT.js

--- src/ChatGPT.js
+++ src/ChatGPT.js
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
     organization: "org-lNKLoHVe60pOjy4RBnctEeEx",
-    apiKey: "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";
+    apiKey: "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"
     //apiKey: process.env.OPENAI_API_KEY,
 });
 
