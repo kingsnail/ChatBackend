5555426c892068e33395865fc4acd6ae5447221d
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-08-19 15:42:17 +0300

    Update ChatGPT.js

--- src/ChatGPT.js
+++ src/ChatGPT.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 async function getChatGPTResponse(promptText) {
   const API_ENDPOINT = 'https://api.openai.com/v1/chat/completions'; // this endpoint might change depending on OpenAI's documentation
-  const API_KEY = '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'; // replace with your OpenAI API key
+  const API_KEY = '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'; // replace with your OpenAI API key
   const ORG_KEY = 'org-lNKLoHVe60pOjy4RBnctEeEx';
   
   const headers = {
