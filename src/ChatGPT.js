664bde1e150a84db6cb40adb08f5609d90d80931
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-08-19 14:03:34 +0300

    Update ChatGPT.js

--- src/ChatGPT.js
+++ src/ChatGPT.js
@@ -1,11 +1,41 @@
-const openai = require('openai');
+const { Configuration, OpenAIApi } = require("openai");
+const readlineSync = require("readline-sync");
+require("dotenv").config();
 
-const configuration = new Configuration({
-    organization: "org-lNKLoHVe60pOjy4RBnctEeEx",
-    apiKey: "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"
-    //apiKey: process.env.OPENAI_API_KEY,
+let APIcall = async () => {
+const newConfig = new Configuration({
+	apiKey: process.env.OPENAI_SECRET_KEY
 });
+const openai = new OpenAIApi(newConfig);
+	
+const chatHistory = [];
 
-const openaiAPI = new OpenAIApi(configuration);
-const response = await openai.listEngines();
+do {
+	const user_input = readlineSync.question("Enter your input: ");
+	const messageList = chatHistory.map(([input_text, completion_text]) => ({
+	role: "user" === input_text ? "ChatGPT" : "user",
+	content: input_text
+	}));
+	messageList.push({ role: "user", content: user_input });
 
+	try {
+	const GPTOutput = await openai.createChatCompletion({
+		model: "gpt-3.5-turbo",
+		messages: messageList,
+	});
+
+	const output_text = GPTOutput.data.choices[0].message.content;
+	console.log(output_text);
+
+	chatHistory.push([user_input, output_text]);
+	} catch (err) {
+	if (err.response) {
+		console.log(err.response.status);
+		console.log(err.response.data);
+	} else {
+		console.log(err.message);
+	}
+	}
+} while (readlineSync.question("\nYou Want more Results? (Y/N)").toUpperCase() === "Y");
+};
+APIcall();
