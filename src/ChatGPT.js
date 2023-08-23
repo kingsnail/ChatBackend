d99c912b942acc243400cec9811b4cdcecaf219c
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-08-23 17:57:42 +0300

    Update ChatGPT.js

--- src/ChatGPT.js
+++ src/ChatGPT.js
@@ -2,13 +2,12 @@ import axios from 'axios';
 
 async function getChatGPTResponse(promptText) {
   const API_ENDPOINT = 'https://api.openai.com/v1/chat/completions'; // this endpoint might change depending on OpenAI's documentation
-  const API_KEY = ''; // replace with your OpenAI API key
+  const API_KEY = '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'; // replace with your OpenAI API key
   const ORG_KEY = '';
   
   const headers = {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer ${API_KEY}`,
-    'OpenAI-Organization': `${ORG_KEY}`
+    'Authorization': `Bearer ${API_KEY}`
   };
 
   console.log("================================");
@@ -16,14 +15,14 @@ async function getChatGPTResponse(promptText) {
   console.log("================================");
   
   const data = {
-     "model": "gpt-3.5-turbo",
-     "messages": [{"role": "user", "content": "Say this is a test!"}],
-     "temperature": 0.7,
-     "max_tokens": 150
+     model: "gpt-3.5-turbo",
+     messages: [{role: "user", content: "Say this is a test!"}],
+     temperature: 0.7,
+     max_tokens: 150
   };
 
   try {
-    const response = await axios.post(API_ENDPOINT, data, { headers: headers });
+    const response = await axios.post(API_ENDPOINT, JSON.stringify(data), { headers: headers });
     return response.data.choices[0].text.trim();
   } catch (error) {
     console.error('Error calling the ChatGPT API:', error);
