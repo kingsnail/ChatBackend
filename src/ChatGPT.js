42115c14e7ebc54a9cd143f83d135518eefc9696
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-08-23 18:30:56 +0300

    Update ChatGPT.js

--- src/ChatGPT.js
+++ src/ChatGPT.js
@@ -6,7 +6,7 @@ class OpenAIAgent{
         this.openai = new OpenAI({apiKey: this.apiKey});
     }
 
-    execute(){
+    async function execute(){
         const completion = await openai.chat.completions.create({
             messages: [{ role: 'user', content: 'Say this is a test' }],
             model: 'gpt-3.5-turbo',
