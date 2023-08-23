6c279bb4baeb4c719ed1c78be7c5d1bccd8ec835
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-08-23 19:00:21 +0300

    Update generator_agent.js

--- src/generator_agent.js
+++ src/generator_agent.js
@@ -1,12 +1,15 @@
 const Agent = require('./agent');
+const OpenAIAgent = require('./ChatGPT');
 
 class GeneratorAgent extends Agent {
-    constructor(name, displayRow, displayCol) {
+    constructor(name, displayRow, displayCol, apiKey) {
         super(name, displayRow, displayCol);
         this.agentType = "generator-agent";   
         this.systemPrompt = `You are a helpful assistant. Please try and follow the next instructions to the best of your ability. When asked a question, answer the question to the best of your ability.`;
         this.userPrompt = "";
         this.outputPrompt = `Format your output as a JSON object with a key value of 'chatResult'. Format 'chatResult' as a JSON list for each response item listed where each list item has the following tags: 'item' whos value is the serial number of the item; and 'text' whos value is the text of the item.`;
+        this.myAgent = new OpenAIAgent(apiKey);
+        
         console.log("New generator agent " + this.name + " UUID: " + this.uuid);
     }
         
