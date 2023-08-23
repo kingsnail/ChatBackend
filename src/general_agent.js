3c18cf19f2c558339b6c6e500672c2aa2f6a5d73
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-08-23 18:42:33 +0300

    Update general_agent.js

--- src/general_agent.js
+++ src/general_agent.js
@@ -1,4 +1,5 @@
 const Agent = require('./agent');
+
 const OpenAIAgent = require('./ChatGPT');
 
 class GeneralAgent extends Agent {
@@ -9,7 +10,7 @@ class GeneralAgent extends Agent {
         this.userPrompt = "";
         this.outputPrompt = `Format your output as a JSON object with a key value of 'chatResult'. Format 'chatResult' as a JSON list for each response item listed where each list item has the following tags: 'item' whos value is the serial number of the item; and 'text' whos value is the text of the item.`;
         this.output = [];
-        this.myAgent = new OpenAIAgent.OpenAIAgent(apiKey);
+        this.myAgent = new OpenAIAgent(apiKey);
         
         console.log("New general agent " + this.name + " UUID: " + this.uuid);
       
