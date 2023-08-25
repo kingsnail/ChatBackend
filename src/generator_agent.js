ed19e4b4324143d324b43bd7d8d61948f8712a6f
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-08-25 21:03:48 +0300

    Update generator_agent.js

--- src/generator_agent.js
+++ src/generator_agent.js
@@ -42,7 +42,7 @@ class GeneratorAgent extends Agent {
 
         (async () => {
         
-            try 
+            try{ 
                 const msg = [{role: 'system', content: this.systemPrompt},
                              {role: 'system', content: this.outputPrompt},
                              {role: 'user',   content: this.userPrompt}
