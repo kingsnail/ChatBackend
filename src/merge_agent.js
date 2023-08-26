3105a8b8881655b953473e4f84afd69148a518ab
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-08-26 18:09:35 +0300

    Create merge_agent.js

---
+++ src/merge_agent.js
@@ -0,0 +1,31 @@
+const Agent = require('./agent');
+
+class MergeAgent extends Agent {
+    constructor(name, displayRow, displayCol) {
+        super(name, displayRow, displayCol);
+        this.agentType = "merge-agent";
+        this.output = [];
+        this.sourcesList = []
+        
+        console.log("New Merge agent " + this.name + " UUID: " + this.uuid);
+      
+    }
+
+    getOutput(){
+        return this.output;
+    } 
+    
+    addSource( s ){
+        this.sourcesList.push( s );
+    }
+  
+    save(){
+        let d = super.save();
+        d['output'] = this.output;
+        console.log('General Agent Save');
+        return d;
+    }
+
+}
+
+module.exports = GeneralAgent;
