092c5fb8d52e62b2d2b144abf4e1f0df77da7a13
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-08-28 21:02:18 +0300

    Update initiator_agent.js

--- src/initiator_agent.js
+++ src/initiator_agent.js
@@ -1,7 +1,7 @@
 const Agent = require('./agent');
 
 class InitiatorAgent extends Agent {
-    constructor(name, displayRow, displayCol) {
+    constructor(agentStore, name, displayRow, displayCol) {
         super(name, displayRow, displayCol);
         this.agentType = "initiator-agent";   
     }
