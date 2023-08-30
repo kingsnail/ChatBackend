e4c6275f68d6706fe720e056f42e3ea878526fe8
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-08-30 10:41:08 +0300

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -7,6 +7,7 @@ class Agent {
         this.name = name;
         this.uuid = uuidv4();
         this.subscribers = [];
+        this.subscriptions = [];
         this.version = 0;
         this.displayRow = displayRow;
         this.displayCol = displayCol;
@@ -68,12 +69,18 @@ class Agent {
         console.log('Subscribe');
     }
 
+    addSubscription(to){
+        this.subscriptions.push( to );
+        console.log("subscription logged");
+    }
+
     load(d){
         console.log('Load');
         this.agentType = d.type;
         this.name = d.name;
         this.uuid = d.uuid;
         this.subscribers = d.subscribers;
+        this.subscriptions = d.subscriptions;
         this.displayRow = d.displayRow;
         this.displayCol = d.displayCol;
         this.version = d.version;
@@ -85,6 +92,7 @@ class Agent {
             name: this.name,
             uuid: this.uuid,
             subscribers: this.subscribers,
+            subscriptions: this.subscriptions,
             displayRow: this.displayRow,
             displayCol: this.displayCol,
             version: this.version,
