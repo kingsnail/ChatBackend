541b2dac10e7359455d0041c8c1cd0ce4c17de8f
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-09-03 19:49:58 +0300

    Update general_agent.js

--- src/general_agent.js
+++ src/general_agent.js
@@ -8,7 +8,8 @@ class GeneralAgent extends Agent {
         this.agentType = "standard-agent";
         this.systemPrompt = `You are a helpful assistant. Please try and follow the next instructions to the best of your ability. When asked a question, answer the question to the best of your ability.`;
         this.userPrompt = "";
-        this.outputPrompt = `You must format all of your output as a JSON object with a key value of 'chatResult'. Format 'chatResult' as a JSON list for each response item listed where each list item has the following tags: 'item' whos value is the serial number of the item; and 'text' whos value is the text of the item.`;
+        this.outputPrompt = `You must format all of your output as a JSON object with a key value of 'chatResult'. Format 'chatResult' as JSON object that has values 'item': 0 and 'text' is to contain all of your response.`;
+        this.outputPromptList = `You must format all of your output as a JSON object with a key value of 'chatResult'. Format 'chatResult' as a JSON list for each response item listed where each list item has the following tags: 'item' whos value is the serial number of the item; and 'text' whos value is the text of the item.`;
         this.output = [];
         this.listItemOutput = false;
     }
