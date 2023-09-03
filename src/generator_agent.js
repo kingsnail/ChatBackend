eeaab5353fd24e2b5462de91f72416461d9e5b55
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-09-03 13:59:16 +0300

    Update generator_agent.js

--- src/generator_agent.js
+++ src/generator_agent.js
@@ -9,6 +9,7 @@ class GeneratorAgent extends Agent {
         this.userPrompt = "Say this is a test";
         this.outputPrompt = `You must format all of your output as a JSON object with a key value of 'chatResult'. Format 'chatResult' as a JSON list for each response item listed where each list item has the following tags: 'item' whos value is the serial number of the item; and 'text' whos value is the text of the item.`;
         this.output = [];
+        this.triggered = false;
         
     }
         
@@ -44,24 +45,28 @@ class GeneratorAgent extends Agent {
     }
 
     execute(){
-
-        (async () => {
+        if (!this.triggered){
+            this.triggered = true;
+            (async () => {
         
-            try{ 
-                const apiKey = this.agentStore.getSessionStore().getApiKeyToUse();
-                const myAgent = new OpenAIAgent(apiKey);
-                const msg = [{role: 'system', content: this.systemPrompt},
-                             {role: 'system', content: this.outputPrompt},
-                             {role: 'user',   content: this.userPrompt}
-                            ];
-                const choices = await myAgent.execute(msg);
-                console.log(choices);
-                this.output.push(choices);
+                try{ 
+                    const apiKey = this.agentStore.getSessionStore().getApiKeyToUse();
+                    const myAgent = new OpenAIAgent(apiKey);
+                    const msg = [{role: 'system', content: this.systemPrompt},
+                                 {role: 'system', content: this.outputPrompt},
+                                 {role: 'user',   content: this.userPrompt}
+                                ];
+                    const choices = await myAgent.execute(msg);
+                    console.log(choices);
+                    this.output.push(choices);
                 
-            } catch (error) {
-                console.error("Failed to fetch data:", error);
-            }
-         })();
+                } catch (error) {
+                    console.error("Failed to fetch data:", error);
+                }
+            })();
+        } else {
+              console.log("Already triggered.");
+        }
     }
 
     save(){
