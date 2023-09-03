8d693b4c39002d85c76b80b903c31f7b4ca6ab02
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-09-03 13:08:09 +0300

    Update general_agent.js

--- src/general_agent.js
+++ src/general_agent.js
@@ -3,18 +3,25 @@ const Agent = require('./agent');
 const OpenAIAgent = require('./ChatGPT');
 
 class GeneralAgent extends Agent {
-    constructor(agentStore, name, displayRow, displayCol, apiKey) {
+    constructor(agentStore, name, displayRow, displayCol) {
         super(agentStore, name, displayRow, displayCol);
         this.agentType = "standard-agent";
         this.systemPrompt = `You are a helpful assistant. Please try and follow the next instructions to the best of your ability. When asked a question, answer the question to the best of your ability.`;
         this.userPrompt = "";
         this.outputPrompt = `You must format all of your output as a JSON object with a key value of 'chatResult'. Format 'chatResult' as a JSON list for each response item listed where each list item has the following tags: 'item' whos value is the serial number of the item; and 'text' whos value is the text of the item.`;
         this.output = [];
-        this.apiKey = apiKey;
-        this.myAgent = new OpenAIAgent(apiKey);
-        
+        this.agentStore = agentStore;
+        this.apiKey = this.agentStore.getApiKey();
+        this.myAgent = new OpenAIAgent(this.apiKey);        
     }
 
+    setApiKey(k) {
+         if (k != this.apiKey){
+              this.apiKey = k;
+              this.myAgent = new OpenAIAgent(this.apiKey);
+         }
+    }
+    
     getSystemPrompt() {
         return this.systemPrompt;
     }
