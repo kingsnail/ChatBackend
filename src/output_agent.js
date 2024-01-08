b482b764fc5490c6baa6be11fcc499c8af905535
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2024-01-08 22:10:04 +0000

    Update output_agent.js

--- src/output_agent.js
+++ src/output_agent.js
@@ -10,7 +10,35 @@ class OutputAgent extends Agent {
         super.setInput(i, fromAgent);
         this.output = this.input;
     }
-    
+       
+    load(d){
+        console.log('Load');
+        this.agentType = d.type;
+        this.name = d.name;
+        this.uuid = d.uuid;
+        this.subscribers = d.subscribers;
+        this.subscriptions = d.subscriptions;
+        this.output = d.output;
+        this.displayRow = d.displayRow;
+        this.displayCol = d.displayCol;
+        this.version = d.version;
+    }
+
+    save(){
+        let d = {
+            type: this.agentType,
+            name: this.name,
+            uuid: this.uuid,
+            subscribers: this.subscribers,
+            subscriptions: this.subscriptions,
+            output: this.output,
+            displayRow: this.displayRow,
+            displayCol: this.displayCol,
+            version: this.version,
+            uuidv:  this.uuid + "|" + this.version.toString()
+        }
+        return d;
+    }
 }
 
 module.exports = OutputAgent;
