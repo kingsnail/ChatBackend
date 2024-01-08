043d8b2fac7980896b5425062a913407711f9ac4
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2024-01-08 21:26:51 +0000

    Update generator_agent.js

--- src/generator_agent.js
+++ src/generator_agent.js
@@ -73,7 +73,7 @@ class GeneratorAgent extends Agent {
                     const completion = await myAgent.execute(msg);
                     const choices = completion.choices;
                     const completionTokens = completion.usage['completion_tokens'];
-                    const propmtTokens     = completion.usage['prompt_tokens'];
+                    const promptTokens     = completion.usage['prompt_tokens'];
                     this.completionTokens  = this.completionTokens + completionTokens;
                     this.promptTokens      = this.promptTokens + promptTokens;
                     console.log("choices[0]=" + JSON.stringify(choices[0]));
