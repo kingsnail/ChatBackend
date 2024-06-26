350a1cd38f4ca56cc3a09bcea4142a7353f8132e
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2024-06-26 12:49:33 +0100

    Update input_agent.js

--- src/input_agent.js
+++ src/input_agent.js
@@ -7,11 +7,28 @@ class InputAgent extends Agent {
         this.inputType = "Text";
     }
 
-setInputType(t) {
+    setInputType(t) {
         this.inputType = t;
         this.version++;
     }
 
+    save(){
+        let d = super.save();
+        
+        return d;
+    }
+    load(d){
+        super.load(d);
+        this.systemPrompt   = d['systemPrompt'];
+        this.userPrompt     = d['userPrompt'];
+        this.outputPrompt   = d['outputPrompt'];
+        this.output         = d['output'];
+        this.apiKey         = d['apiKey'];        
+        this.listItemOutput = d['listItemOutput'];
+    }
+}
+
+    
 }
 
 module.exports = InputAgent;
