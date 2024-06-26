31b6f25d0eca7cf33a957c02f61ebc9618b564df
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2024-06-26 10:54:14 +0100

    Update input_agent.js

--- src/input_agent.js
+++ src/input_agent.js
@@ -3,7 +3,8 @@ const Agent = require('./agent');
 class InputAgent extends Agent {
     constructor(agentStore, name, displayRow, displayCol) {
         super(agentStore, name, displayRow, displayCol);
-        this.agentType = "input-agent";   
+        this.agentType = "input-agent";
+        this.inputType = "Text";
     }
         
 }
